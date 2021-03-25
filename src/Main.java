b40d9a4b9e7b63c5613a109932dea63bfd18cbf0
Yyertsenoj94 <51996493+Yyertsenoj94@users.noreply.github.com>
2021-03-25 09:20:12 -0500

    Adding print ingredients fxml, image, controller class, and folder for recipes

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-//Joseph Galvan: QRK815: CS 3443-003: Heena Rathore
+/*
+CS 3443-003: Heena Rathore
+Joseph Galvan: QRK815
+Trey Jones: LVS888
+
+ */
 
 public class Main extends Application {
     //Buttons already change the scenes
     @Override
     public void start(Stage primaryStage) throws Exception{
-        AnchorPane root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        //adding multiple lines for each person's menu temporarily until we can switch menus with a button.
+
+        //AnchorPane root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        AnchorPane root = FXMLLoader.load(getClass().getResource("PrintGroceryList.fxml"));
+        //AnchorPane root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+
         Scene scene = new Scene(root, 750, 500);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Grocer-Eaze: Main");
